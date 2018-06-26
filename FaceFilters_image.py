9eaed2a161078328734150b6e5b7d33a94031e76
LoppA <xLopacheco@gmail.com>
2018-06-26 04:10:32 -0300

    mustache mask

--- FaceFilters_image.py
+++ FaceFilters_image.py
@@ -2,6 +2,7 @@ import cv2
 import dlib
 import time
 import numpy as np
+from math import asin, pi
 
 # Classe para representar um ponto/vetor, facilitar codigo para as funcoes de operacoes geometricas
 class Point:
@@ -52,6 +53,73 @@ def dot(a, b):
 def draw_line(img, a, b):
     cv2.line(img, a.tuple(), b.tuple(), color=(0, 0, 255), thickness=2)
 
+def put_debug(img, landmarks):
+    landmarks_display = landmarks[RIGHT_EYE_POINTS + LEFT_EYE_POINTS + NOSE_POINTS + MOUTH_INNER_POINTS]
+
+    # Reta que liga os 2 olhos
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    point_inter = point_mouth.intersect_line(point_left, point_right)
+#    draw_line(img, point_left, point_right)
+#    draw_line(img, point_mouth, point_inter)
+
+#    for idx, point in enumerate(landmarks_display):
+#        pos = (point[0, 0], point[0, 1])
+#        cv2.circle(img, pos, 2, color=(0, 255, 255), thickness=-1)
+
+def fix(p, lim):
+    if p < 0:
+        return 0
+    if p >= lim:
+        return lim-1
+    return p
+
+def put_mustache(img, landmarks, x, y, w, h):
+    mask_img = cv2.imread('filters/mustache.png', -1)
+
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+
+    line = point_right - point_left
+
+    ang = asin(line.y / line.x) * 180.0 / pi
+
+    rows, cols, _ = mask_img.shape
+    M = cv2.getRotationMatrix2D((cols/2, rows/2), -ang, 1)
+    mask_img = cv2.warpAffine(mask_img, M, (cols, rows))
+
+    ori_mask = mask_img[:,:,3]
+    ori_mask_inv = cv2.bitwise_not(ori_mask)
+    mask_img = mask_img[:,:,0:3]
+
+    mouth_point = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    nose_point = Point(landmarks[UNDER_NOSE_POINT, 0], landmarks[UNDER_NOSE_POINT, 1])
+    center = mouth_point + ((nose_point - mouth_point)*(1/3))   # Altura do bigode
+
+                        #width do bigode
+    x1 = int(center.x-(0.7*w/2))
+    x2 = int(center.x+(0.7*w/2))
+                       #height do bigode
+    y1 = int(center.y-0.2*h/2.0)
+    y2 = int(center.y+0.2*h/2.0)
+
+    x1 = fix(x1, img.shape[1])
+    x2 = fix(x2, img.shape[1])
+    y1 = fix(y1, img.shape[0])
+    y2 = fix(y2, img.shape[0])
+
+    mask_overlay = cv2.resize(mask_img, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+    mask = cv2.resize(ori_mask, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+    mask_inv = cv2.resize(ori_mask_inv, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+
+    roi = img[y1:y2, x1:x2]
+
+    roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+    roi_fg = cv2.bitwise_and(mask_overlay, mask_overlay, mask = mask)
+    dst = cv2.add(roi_bg,roi_fg)
+    img[y1:y2, x1:x2] = dst
+
 # Definição dos pontos referentes a cada parte do rosto
 NOSE_POINTS = list(range(27, 36))  
 RIGHT_EYE_POINTS = list(range(36, 42))  
@@ -60,14 +128,15 @@ MOUTH_INNER_POINTS = list(range(61, 68))
 
 LEFT_EYE_POINT = 36
 RIGHT_EYE_POINT = 45
-CENTER_MOUTH_POINT = 62
+CENTER_MOUTH_POINT = 63
+UNDER_NOSE_POINT = 32
 
 # Carregando classificador de faces e landmarks
 face_cascade = cv2.CascadeClassifier('data/lbpcascade_frontalface.xml')
 predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')
 
 # Leitura da imagem
-ori_img = cv2.imread('images/guys.png')
+ori_img = cv2.imread('images/group_2.jpg')
 
 # Transformando a imagem para escala de cinza
 gray = cv2.cvtColor(ori_img, cv2.COLOR_BGR2GRAY)
@@ -77,22 +146,12 @@ faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
 # Para cada face, coloca um retângulo em volta e identifica olhos, nariz e boca
 for (x, y, w, h) in faces:
-    cv2.rectangle(ori_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#    cv2.rectangle(ori_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
     dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
     landmarks = np.matrix([[p.x, p.y] for p in predictor(ori_img, dlib_rect).parts()])
-    landmarks_display = landmarks[RIGHT_EYE_POINTS + LEFT_EYE_POINTS + NOSE_POINTS + MOUTH_INNER_POINTS]
-
-    # Reta que liga os 2 olhos
-    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
-    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
-    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
-    point_inter = point_mouth.intersect_line(point_left, point_right)
-    draw_line(ori_img, point_left, point_right)
-    draw_line(ori_img, point_mouth, point_inter)
-
-    for idx, point in enumerate(landmarks_display):
-        pos = (point[0, 0], point[0, 1])
-        cv2.circle(ori_img, pos, 2, color=(0, 255, 255), thickness=-1)
+   
+    put_debug(ori_img, landmarks)
+    put_mustache(ori_img, landmarks, x, y, w, h)
 
 
 # Mostrando a imagem
