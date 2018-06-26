2e3a8d500f01bdcd4c2a9f1e713f76ad8133a371
Guilherme Tubone <ghtubone@gmail.com>
2018-06-26 02:28:47 -0300

    Primeira sobreposição de filtro funcionando

--- FaceFilters_webcam.py
+++ FaceFilters_webcam.py
@@ -52,6 +52,37 @@ def dot(a, b):
 def draw_line(img, a, b):
     cv2.line(img, a.tuple(), b.tuple(), color=(0, 0, 255), thickness=2)
 
+def put_mask(img, mid_eye, dist):
+	mask_img = cv2.imread('filters/eye_mask.png', -1)
+	ori_mask = mask_img[:,:,3]
+	ori_mask_inv = cv2.bitwise_not(ori_mask)
+	mask_img = mask_img[:,:,0:3]
+
+	x1 = int(mid_eye.x-(dist/2))
+	x2 = int(mid_eye.x+(dist/2))
+	y1 = int(mid_eye.y-100)
+	y2 = int(mid_eye.y+100)
+
+	if x1 < 0:
+		x1 = 0
+	if y1 < 0:
+		y1 = 0
+	if x2 > img.shape[0]:
+		x2 = img.shape[0]
+	if y2 > img.shape[1]:
+		y2 = img.shape[1]
+
+	mask_overlay = cv2.resize(mask_img, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+	mask = cv2.resize(ori_mask, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+	mask_inv = cv2.resize(ori_mask_inv, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+
+	roi = img[y1:y2, x1:x2]
+
+	roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+	roi_fg = cv2.bitwise_and(mask_overlay, mask_overlay, mask = mask)
+	dst = cv2.add(roi_bg,roi_fg)
+	img[y1:y2, x1:x2] = dst
+
 # Definição dos pontos referentes a cada parte do rosto
 NOSE_POINTS = list(range(27, 36))  
 RIGHT_EYE_POINTS = list(range(36, 42))  
@@ -80,7 +111,7 @@ while(True):
 
         # Para cada face, coloca um retângulo em volta e identifica olhos, nariz e boca
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
         landmarks = np.matrix([[p.x, p.y] for p in predictor(frame, dlib_rect).parts()])
         landmarks_display = landmarks[RIGHT_EYE_POINTS + LEFT_EYE_POINTS + NOSE_POINTS + MOUTH_INNER_POINTS]
@@ -89,12 +120,14 @@ while(True):
         point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
         point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
         point_inter = point_mouth.intersect_line(point_left, point_right)
-        draw_line(frame, point_left, point_right)
-        draw_line(frame, point_mouth, point_inter)
+        #draw_line(frame, point_left, point_right)
+        #draw_line(frame, point_mouth, point_inter)
 
         for idx, point in enumerate(landmarks_display):
             pos = (point[0, 0], point[0, 1])
-            cv2.circle(frame, pos, 2, color=(0, 255, 255), thickness=-1)
+            #cv2.circle(frame, pos, 2, color=(0, 255, 255), thickness=-1)
+
+            put_mask(frame, point_inter, np.sqrt((point_left.x-point_right.x)**2 + (point_left.y-point_right.y)**2))
 
     # Mostrando frame
     cv2.imshow('frame',frame)
