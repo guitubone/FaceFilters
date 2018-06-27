458532ef1ab67d131aaad1ed941f44a880a6e30b
LoppA <xLopacheco@gmail.com>
2018-06-27 00:34:53 -0300

    webcam updated

--- FaceFilters_webcam.py
+++ FaceFilters_webcam.py
@@ -2,6 +2,7 @@ import cv2
 import dlib
 import time
 import numpy as np
+from math import asin, pi
 
 # Classe para representar um ponto/vetor, facilitar codigo para as funcoes de operacoes geometricas
 class Point:
@@ -49,38 +50,91 @@ class Point:
 def dot(a, b):
     return a.x*b.x + a.y*b.y
 
+class Filter:
+    def __init__(self, file_path, get_center, w_ratio, h_ratio):
+        self.file_path = file_path
+        self.get_center = get_center
+        self.w_ratio = w_ratio
+        self.h_ratio = h_ratio
+
+    def put(self, img, landmarks, w, h, x, y):
+        mask_img = cv2.imread(self.file_path, -1)
+
+        ang = get_ang(landmarks)
+
+        rows, cols, _ = mask_img.shape
+        M = cv2.getRotationMatrix2D((cols/2, rows/2), -ang, 1)
+        mask_img = cv2.warpAffine(mask_img, M, (cols, rows))
+
+        ori_mask = mask_img[:,:,3]
+        ori_mask_inv = cv2.bitwise_not(ori_mask)
+        mask_img = mask_img[:,:,0:3]
+
+        center = self.get_center(landmarks, x, y)
+
+        x1 = int(center.x-(self.w_ratio*w/2.0))
+        x2 = int(center.x+(self.w_ratio*w/2.0))
+
+        y1 = int(center.y-self.h_ratio*h/2.0)
+        y2 = int(center.y+self.h_ratio*h/2.0)
+
+        x1 = fix(x1, img.shape[1])
+        x2 = fix(x2, img.shape[1])
+        y1 = fix(y1, img.shape[0])
+        y2 = fix(y2, img.shape[0])
+
+        mask_overlay = cv2.resize(mask_img, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+        mask = cv2.resize(ori_mask, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+        mask_inv = cv2.resize(ori_mask_inv, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+
+        roi = img[y1:y2, x1:x2]
+
+        roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+        roi_fg = cv2.bitwise_and(mask_overlay, mask_overlay, mask = mask)
+        dst = cv2.add(roi_bg,roi_fg)
+        img[y1:y2, x1:x2] = dst
+
+def get_ang(landmarks):
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+
+    line = point_right - point_left
+
+    return asin(line.y / line.x) * 180.0 / pi
+
 def draw_line(img, a, b):
     cv2.line(img, a.tuple(), b.tuple(), color=(0, 0, 255), thickness=2)
 
-def put_mask(img, mid_eye, dist, mask_img):
-	ori_mask = mask_img[:,:,3]
-	ori_mask_inv = cv2.bitwise_not(ori_mask)
-	mask_img = mask_img[:,:,0:3]
+def put_debug(img, landmarks, x, y, w, h):
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    landmarks_display = landmarks[RIGHT_EYE_POINTS + LEFT_EYE_POINTS + NOSE_POINTS + MOUTH_INNER_POINTS]
 
-	x1 = int(mid_eye.x-(dist/2))
-	x2 = int(mid_eye.x+(dist/2))
-	y1 = int(mid_eye.y-100)
-	y2 = int(mid_eye.y+100)
+    # Reta que liga os 2 olhos
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    point_inter = point_mouth.intersect_line(point_left, point_right)
+    draw_line(img, point_left, point_right)
+    draw_line(img, point_mouth, point_inter)
 
-	if x1 < 0:
-		x1 = 0
-	if y1 < 0:
-		y1 = 0
-	if x2 > img.shape[0]:
-		x2 = img.shape[0]
-	if y2 > img.shape[1]:
-		y2 = img.shape[1]
+    for idx, point in enumerate(landmarks_display):
+        pos = (point[0, 0], point[0, 1])
+        cv2.circle(img, pos, 2, color=(0, 255, 255), thickness=-1)
 
-	mask_overlay = cv2.resize(mask_img, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
-	mask = cv2.resize(ori_mask, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
-	mask_inv = cv2.resize(ori_mask_inv, (x2-x1, y2-y1), interpolation = cv2.INTER_AREA)
+def fix(p, lim):
+    if p < 0:
+        return 0
+    if p >= lim:
+        return lim-1
+    return p
 
-	roi = img[y1:y2, x1:x2]
+def put_blur(img, rect):
+	x1, y1, x2, y2 = rect
 
-	roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
-	roi_fg = cv2.bitwise_and(mask_overlay, mask_overlay, mask = mask)
-	dst = cv2.add(roi_bg,roi_fg)
-	img[y1:y2, x1:x2] = dst
+	img_rect = img[y1:y2, x1:x2]
+	img_rect = cv2.blur(img_rect, (25, 25))
+
+	img[y1:y2, x1:x2] = img_rect
 
 # Definição dos pontos referentes a cada parte do rosto
 NOSE_POINTS = list(range(27, 36))  
@@ -90,7 +144,59 @@ MOUTH_INNER_POINTS = list(range(61, 68))
 
 LEFT_EYE_POINT = 36
 RIGHT_EYE_POINT = 45
-CENTER_MOUTH_POINT = 62
+CENTER_MOUTH_POINT = 63
+UNDER_NOSE_POINT = 32
+NOSE_POINT = 30
+UP_TONGUE_POINT = 62
+BOT_TONGUE_POINT = 66
+LEFT_EAR_POINT = 17
+RIGHT_EAR_POINT = 26
+
+def get_center_mustache(landmarks, x, y):
+    mouth_point = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    nose_point = Point(landmarks[UNDER_NOSE_POINT, 0], landmarks[UNDER_NOSE_POINT, 1])
+    center = mouth_point + ((nose_point - mouth_point)*(1.0/3.0))   # Altura do bigode
+    return center
+Mustache = Filter('filters/mustache.png', get_center_mustache, 0.7, 0.2)
+
+def get_center_flower_crown(landmarks, x, y):
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    center = point_left + ((point_right - point_left)*(1.0/2.0))   # Altura do bigode
+    return Point(center.x, y)
+FlowerCrown = Filter('filters/flower_crown.png', get_center_flower_crown, 1.2, 0.6)
+
+def get_center_dog_nose(landmarks, x, y):
+    return Point(landmarks[NOSE_POINT, 0], landmarks[NOSE_POINT, 1])
+DogNose = Filter('filters/dog_nose.png', get_center_dog_nose, 0.4, 0.3)
+
+# TODO
+def get_center_dog_tongue(landmarks, x, y):
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    point_inter = point_mouth.intersect_line(point_left, point_right)
+    p = Point(landmarks[BOT_TONGUE_POINT, 0], landmarks[BOT_TONGUE_POINT, 1])
+    return p + (point_mouth - point_inter)*0.1
+DogTongue = Filter('filters/dog_tongue.png', get_center_dog_tongue, 0.4, 0.3)
+
+def get_center_dog_left_ear(landmarks, x, y):
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    point_inter = point_mouth.intersect_line(point_left, point_right)
+    p = Point(landmarks[LEFT_EAR_POINT,0], landmarks[LEFT_EAR_POINT, 1])
+    return p + (point_inter - point_mouth)*1.0
+DogLeftEar = Filter('filters/dog_left_ear.png', get_center_dog_left_ear, 0.4, 0.3)
+
+def get_center_dog_right_ear(landmarks, x, y):
+    point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
+    point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
+    point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
+    point_inter = point_mouth.intersect_line(point_left, point_right)
+    p = Point(landmarks[RIGHT_EAR_POINT,0], landmarks[RIGHT_EAR_POINT, 1])
+    return p + (point_inter - point_mouth)*1.0
+DogRightEar = Filter('filters/dog_right_ear.png', get_center_dog_right_ear, 0.4, 0.3)
 
 # Carregando classificador de faces e landmarks
 face_cascade = cv2.CascadeClassifier('data/lbpcascade_frontalface.xml')
@@ -107,27 +213,21 @@ while(True):
 
 	# Detecção das faces
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-    mask_img = cv2.imread('filters/eye_mask.png', -1)
 
         # Para cada face, coloca um retângulo em volta e identifica olhos, nariz e boca
     for (x, y, w, h) in faces:
-        #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
         landmarks = np.matrix([[p.x, p.y] for p in predictor(frame, dlib_rect).parts()])
-        landmarks_display = landmarks[RIGHT_EYE_POINTS + LEFT_EYE_POINTS + NOSE_POINTS + MOUTH_INNER_POINTS]
 
-        point_left = Point(landmarks[LEFT_EYE_POINT, 0], landmarks[LEFT_EYE_POINT, 1])
-        point_right = Point(landmarks[RIGHT_EYE_POINT, 0], landmarks[RIGHT_EYE_POINT, 1])
-        point_mouth = Point(landmarks[CENTER_MOUTH_POINT, 0], landmarks[CENTER_MOUTH_POINT, 1])
-        point_inter = point_mouth.intersect_line(point_left, point_right)
-        #draw_line(frame, point_left, point_right)
-        #draw_line(frame, point_mouth, point_inter)
+#        put_blur(frame, [int(x), int(y), int(x+w), int(y+h)])
 
-        for idx, point in enumerate(landmarks_display):
-            pos = (point[0, 0], point[0, 1])
-            #cv2.circle(frame, pos, 2, color=(0, 255, 255), thickness=-1)
-
-            put_mask(frame, point_inter, np.sqrt((point_left.x-point_right.x)**2 + (point_left.y-point_right.y)**2), mask_img)
+#        put_debug(frame, landmarks, x, y, w, h)
+        Mustache.put(frame, landmarks, w, h, x, y)
+#FlowerCrown.put(frame, landmarks, w, h, x, y#)
+#        DogNose.put(frame, landmarks, w, h, x, y)
+#        DogTongue.put(frame, landmarks, w, h, x, y)
+        DogLeftEar.put(frame, landmarks, w, h, x, y)
+        DogRightEar.put(frame, landmarks, w, h, x, y)
 
     # Mostrando frame
     cv2.imshow('frame',frame)
