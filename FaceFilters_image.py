b0390df37828ea7698652b78dff6795e6a78d589
LoppA <xLopacheco@gmail.com>
2018-06-28 00:43:00 -0300

    FaceFilter_image keyboard arrow comands

--- FaceFilters_image.py
+++ FaceFilters_image.py
@@ -5,36 +5,66 @@ import time
 import numpy as np
 from math import asin, pi
 
+LEFT_ARROW = 65361
+UP_ARROW = 65362
+DOWN_ARROW = 65364
+RIGHT_ARROW = 65363
+NUM_FILTES = 5
+
 # Carregando classificador de faces e landmarks
 face_cascade = cv2.CascadeClassifier('data/lbpcascade_frontalface.xml')
 predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')
 
-# Leitura da imagem
-ori_img = cv2.imread('images/guys.png')
+id = 0
+while(True):
+    # Leitura da imagem
+	ori_img = cv2.imread('images/guys.png')
 
-# Transformando a imagem para escala de cinza
-gray = cv2.cvtColor(ori_img, cv2.COLOR_BGR2GRAY)
+    # Transformando a imagem para escala de cinza
+	gray = cv2.cvtColor(ori_img, cv2.COLOR_BGR2GRAY)
 
-# Detecção das faces
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Detecção das faces
+	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-# Para cada face, coloca um retângulo em volta e identifica olhos, nariz e boca
-for (x, y, w, h) in faces:
-    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
-    landmarks = np.matrix([[p.x, p.y] for p in predictor(ori_img, dlib_rect).parts()])
+    # Para cada face, coloca um retângulo em volta e identifica olhos, nariz e boca
+	for (x, y, w, h) in faces:
+		dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+		landmarks = np.matrix([[p.x, p.y] for p in predictor(ori_img, dlib_rect).parts()])
 
-#    FF.put_debug(ori_img, landmarks, x, y, w, h)
-#    FF.Mustache.put(ori_img, landmarks, w, h, x, y)
-#    FF.FlowerCrown.put(ori_img, landmarks, w, h, x, y)
-#    FF.DogNose.put(ori_img, landmarks, w, h, x, y)
-#    if(FF.mouth_open(landmarks, w, h)):
-#        FF.DogTongue.put(ori_img, landmarks, w, h, x, y)
-#    FF.DogLeftEar.put(ori_img, landmarks, w, h, x, y)
-#    FF.DogRightEar.put(ori_img, landmarks, w, h, x, y)
-    FF.Glasses.put(ori_img, landmarks, w, h, x, y)
+		if id == -1:
+			FF.put_debug(ori_img, landmarks, x, y, w, h)
+		elif id == -2:
+			ori_img = FF.put_blur(ori_img, landmarks, x, y, w, h)
+		elif id == 1:
+			FF.Mustache.put(ori_img, landmarks, w, h, x, y)
+		elif id == 2:
+			FF.FlowerCrown.put(ori_img, landmarks, w, h, x, y)
+		elif id == 3:
+			FF.DogNose.put(ori_img, landmarks, w, h, x, y)
+			if(FF.mouth_open(landmarks, w, h)):
+				FF.DogTongue.put(ori_img, landmarks, w, h, x, y)
+			FF.DogLeftEar.put(ori_img, landmarks, w, h, x, y)
+			FF.DogRightEar.put(ori_img, landmarks, w, h, x, y)
+		elif id == 4:
+			FF.Glasses.put(ori_img, landmarks, w, h, x, y)
 
-#    ori_img = FF.put_blur(ori_img, landmarks, x, y, w, y)
+	# Mostrando a imagem
+	cv2.imshow('image', ori_img)
 
-# Mostrando a imagem
-cv2.imshow('image', ori_img)
-cv2.waitKey(0)
+	key = cv2.waitKeyEx(0)
+	if key == ord('q') or key == ord('Q'):
+		break
+	elif key == LEFT_ARROW:
+		if id < 0:
+			id = 0
+		id = (id-1)%NUM_FILTES
+	elif key == RIGHT_ARROW:
+		if id < 0:
+			id = 0
+		id = (id+1)%NUM_FILTES
+	elif key == UP_ARROW:
+		id = -2
+	elif key == DOWN_ARROW:
+		id = -1
+
+cv2.destroyAllWindows()
